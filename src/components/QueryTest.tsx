a398d4ee9052bf4a8a8c1a528b79fd2fcbae838c
cocolemon619 <torikuto.66@gmal.com>
2024-03-27 13:41:00 +0900

    map関数にkeyを追加

--- src/components/QueryTest.tsx
+++ src/components/QueryTest.tsx
@@ -10,23 +10,12 @@ const Users = ({ users }) => {
     return (
         <div>
             <h1>ユーザーリスト</h1>
-            {/* <ul className='menu menu-vertical lg:menu-horizontal bg-base-200 rounded-box'>
-                {users.map((user) => (
-                    <li key={user.id}>
-                        <p>ID: {user.id}</p>
-                        <p>ユーザー名: {user.name}</p>
-                        <p>メールアドレス {user.mail}</p>
-                        <p>パスワード: {user.password}</p>
-                    </li>
-                ))}
-            </ul> */}
             {users.map((user) => (
-                <ul className='menu menu-vertical lg:menu-horizontal bg-base-200 rounded-box w-64 m-1 shadow'>
-                    <li key={user.id}>
+                <ul key={user.id} className='menu menu-vertical lg:menu-horizontal bg-base-200 rounded-box w-64 m-1 shadow'>
+                    <li>
                         <p>ID: {user.id}</p>
                         <p>ユーザー名: {user.name}</p>
                         <p>メールアドレス: {user.mail}</p>
-                        {/* <p>パスワード: {user.password}</p> */}
                     </li>
                 </ul>
             ))}
