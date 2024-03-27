97e39699dc128c5aaca0be060c1745d052ff3fe9
cocolemon619 <torikuto.66@gmal.com>
2024-03-28 06:45:14 +0900

    node_modulesの修正

---
+++ src/pages/test.tsx
@@ -0,0 +1,22 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+async function main() {
+    const users = await prisma.user.findMany();
+    console.log(users);
+}
+
+main()
+    .catch(e => {
+        throw e;
+    })
+    .finally(async () => {
+        await prisma.$disconnect();
+    });
+
+    export default function SortTest(){
+        return(
+            <div>
+            </div>
+        )
+    }
